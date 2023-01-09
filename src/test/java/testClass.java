02970e6cfe207eacf9b2c91bbc11f13bed7ecad2
AhmedAlaaEldien <46040249+AhmedAlaaEldien@users.noreply.github.com>
2023-01-09 16:35:41 +0200

    Add files via upload

---
+++ src/test/java/testClass.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class testClass extends mClass{
+
+    @Test
+    public void printClass(){
+        ppClass();
+    }
+
+    @Test
+    public void printClass2(){
+        System.out.println("Hello World");
+    }
+}
